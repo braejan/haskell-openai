3a42920ce067ccef883206c251f47c577bbf6c11
Braejan Arias <barias@sonatype.com>
2022-12-31 09:43:59 -0500

    :white_check_mark: Unit test for Choice data type.

--- openai-types/test/Main.hs
+++ openai-types/test/Main.hs
@@ -1,12 +1,14 @@
 module Main(main) where
 import Test.Tasty (TestTree, defaultMain, testGroup)
 import OpenAI.Types.RequestTest
+import OpenAI.Types.ChoiceTest
 
 -- Test suite definition
 tests :: TestTree
 tests =
   testGroup "Test suite for Module openai-types"
     [ allRequestTest
+     ,allChoiceTest
     ]
 
 main :: IO ()
