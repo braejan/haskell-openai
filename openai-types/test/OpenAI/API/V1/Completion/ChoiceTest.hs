9495d09ebb8bd8c0baa6a88c6cb2337766eff8fb
braejan <ing.brayanarias@gmail.com>
2023-02-15 09:54:01 -0500

    :white_check_mark: Choice refactored.

--- openai-types/test/OpenAI/API/V1/Completion/ChoiceTest.hs
+++ openai-types/test/OpenAI/API/V1/Completion/ChoiceTest.hs
@@ -21,10 +21,10 @@ allCompletionChoiceTest =
     , testEmptyString --2
     , testRequiredFields --3
     , testAllFields --4
-    -- , testExtraFields --5
-    -- , testInvalidTextType --6
-    -- , testInvalidIndexType --7
-    -- , testValidChoice --8
+    , testExtraFields --5
+    , testInvalidTextType --6
+    , testInvalidIndexType --7
+    , testCreateCompletionChoice --8
     ]
 
 
@@ -55,9 +55,31 @@ testAllFields = testCase "Deserialization of complete JSON to Choice should resu
       actualChoice = decode (fromString input) :: Maybe Choice
   assertEqual "Test 4: Deserialized value should match expected Choice value" (Just expectedChoice) actualChoice
 
--- -- Test case 5:
--- testExtraFields :: TestTree
--- testExtraFields = testCase "Deserialization of JSON with extra fields to Choice should result in an error." $ do
---   let input = "{\"text\":\"hello\",\"index\":0,\"logprobs\":[0.0,0.0],\"finish_reason\":\"length\",\"extra_field\":\"extra_value\"}"
---       choice' = decode (fromString input) :: Maybe Choice
---   assertBool "Test 5: Deserializing JSON with extra fields to Choice should result in an error." (isNothing choice')
+-- Test case 5:
+testExtraFields :: TestTree
+testExtraFields = testCase "Deserialization of JSON with extra fields to Choice should result in a valid Choice." $ do
+  let input = "{\"text\":\"Which color do you prefer?\",\"index\":0,\"logprobs\":[0.1,0.2],\"finish_reason\":\"length\",\"extra_field\":\"extra_value\"}"
+      expectedChoice = Choice "Which color do you prefer?" 0 (Just [0.1,0.2]) "length"
+      actualChoice = decode (fromString input) :: Maybe Choice
+  assertEqual "Test 5: Deserializing JSON with extra fields to Choice should result in a valid Choice." (Just expectedChoice) actualChoice
+
+-- Test case 6:
+testInvalidTextType :: TestTree
+testInvalidTextType = testCase "Deserialization of JSON with invalid type for text field to Choice should result in an error." $ do
+  let input = "{\"text\":0,\"index\":0,\"logprobs\":[0.1,0.2],\"finish_reason\":\"length\"}"
+      choice' = decode (fromString input) :: Maybe Choice
+  assertBool "Test 6: Deserializing JSON with invalid type for text field to Choice should result in an error." (isNothing choice')
+
+-- Test case 7:
+testInvalidIndexType :: TestTree
+testInvalidIndexType = testCase "Deserialization of JSON with invalid type for index field to Choice should result in an error." $ do
+  let input = "{\"text\":\"Which color do you prefer?\",\"index\":\"0\",\"logprobs\":[0.1,0.2],\"finish_reason\":\"length\"}"
+      choice' = decode (fromString input) :: Maybe Choice
+  assertBool "Test 7: Deserializing JSON with invalid type for index field to Choice should result in an error." (isNothing choice')
+
+-- Test case 8:
+testCreateCompletionChoice :: TestTree
+testCreateCompletionChoice = testCase "Create a valid Choice." $ do
+  let expectedChoice = Choice "" 0 Nothing ""
+      actualChoice = createCompletionChoice 
+  assertEqual "Test 8: Choice created should match expected Choice value" expectedChoice actualChoice
